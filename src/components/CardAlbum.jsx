b2e67485eb0b43b3a092224a37377f600f79c8e6
Yazid Rahmouni <yezidrahmouni@gmail.com>
2021-02-05 18:35:06 +0300

    initial commit

--- src/components/CardAlbum.jsx
+++ src/components/CardAlbum.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import { Card, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 const CardAlbum = ({ album }) => {
   return (
-   
-      <Card>
-        <Card.Img variant="top" src={album.cover} />
-        <Card.Body>
-          <Card.Title>{album.title}</Card.Title>
-        </Card.Body>
-      </Card>
+    <Card>
+      <Card.Img variant="top" src={album.cover} />
+      <Card.Body>
+        <Card.Title>{album.title}</Card.Title>
+        <FontAwesomeIcon icon={faHeart} className="far" id="Heart" />
+      </Card.Body>
+    </Card>
   );
 };
 
